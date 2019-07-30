dc6ee2da1d55d7baa5120d8b775b381cd820c4ec
Stephen-kongxiangwei <53067722+Stephen-kongxiangwei@users.noreply.github.com>
2019-07-30 09:17:43 +0800

    Update max7219.ts

--- max7219.ts
+++ max7219.ts
@@ -1,7 +1,9 @@
 //% weight=120 color=#006d19 icon="\uf00" block="MAX7219 8x4"
 namespace max7219_matrix{
-const _NOOP=0
-const _DIGIT=[1,2,3,4,5,6,7,8]
+const _NOOP=0//没有任何操作，不改变当前状态
+const _DIGIT=[1,2,3,4,5,6,7,8]//
+const _DECODEMODE=9
+const _IN
 
 
 
